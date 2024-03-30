bdffe8abf3a534166b97521fcc80ebcb899189e6
Muhammet Ozdemir <65421785+mr-ozdemir@users.noreply.github.com>
2024-03-30 15:17:21 +0300

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -9,6 +9,9 @@ class CustomDataset(Dataset):
         self.dataframe = dataframe
         self.img_dir = img_dir
         self.transform = transform
+        self.dataframe['class'] -= 1
+        
+
 
     def __len__(self):
         return len(self.dataframe)
@@ -19,31 +22,15 @@ class CustomDataset(Dataset):
         image = Image.open(img_name).convert('RGB')
 
         xmin, xmax, ymin, ymax = self.dataframe.iloc[idx][['xmin', 'xmax', 'ymin', 'ymax']].values
-        original_size = image.size  # Original size (width, height)
-
-        scale_width = 720 / original_size[0]
-        scale_height = 720 / original_size[1]
-
-        xmin_norm = int(xmin * scale_width)
-        xmax_norm = int(xmax * scale_width)
-        ymin_norm = int(ymin * scale_height)
-        ymax_norm = int(ymax * scale_height)
-
-        # Crop and resize the image
-        image = image.crop((xmin_norm, ymin_norm, xmax_norm, ymax_norm))
         
+        # Crop and resize the image
+        image = image.crop((xmin, ymin, xmax, ymax))
+     
         # Apply the transform after cropping
         if self.transform:
             image = self.transform(image)
-        else:
-            # Default transform if none specified
-            resize_transform = transforms.Compose([
-                transforms.Resize((224, 224)),  # Resize the cropped region to a fixed size
-                transforms.ToTensor(),
-            ])
-            image = resize_transform(image)
 
-        class_id = self.dataframe.iloc[idx]['class']
+        class_id = self.dataframe.iloc[idx]['class']  # This will now be correctly zero-indexed
         label = torch.tensor(class_id, dtype=torch.long)
 
         return {'image': image, 'label': label}
